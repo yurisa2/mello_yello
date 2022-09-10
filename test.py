477dc37478a11d0d84bc12b0299cea9d74a95704
@yurisa2 - SJ <yuri@sa2.com.br>
2022-09-09 23:36:17 -0300

    Ponstan

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import time
+import logging
 
 def main():
     while True:
         print('El Time: ', time.time())
+        logging.info(time.time())
         time.sleep(1)
 
 if __name__ == '__main__':
